0e293b782f81c63c38087258303f0c4add1bc599
Rounak kumar <rouanakraj6201@gmail.com>
2026-07-22 12:36:56 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("", include("accounts.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
